c24b1c4caff7ba40de7795226512b3fc4d4f0d44
Alejandro Velasco <alejandrovelasco94@gmail.com>
2017-11-16 18:20:32 -0600

    android project added

---
+++ android/app/src/main/java/com/labcontrol/MainActivity.java
@@ -0,0 +1,17 @@
+package com.labcontrol;
+
+import com.facebook.react.ReactActivity;
+
+import in.sriraman.sharedpreferences.RNSharedPreferencesReactPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "LabControl";
+    }
+}
